1b80fb7806e24f70519af73256247d4503a40c5a
DaQiangSu <13683804270@163.com>
2020-05-11 16:51:08 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -30,6 +30,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:DaQiangSu/vuepress-docs.git gh-pages:gh-pages
+#git push -f git@github.com:DaQiangSu/vuepress-docs.git master:gh-pages
+git push -f origin master:gh-pages
 
 cd -
