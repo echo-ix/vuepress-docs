c8fa672ea6f9e0fe6a1902d376e81b0022acc29d
DaQiangSu <13683804270@163.com>
2020-05-11 17:20:11 +0800

    更新功能

--- deploy.sh
+++ deploy.sh
@@ -3,35 +3,29 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-#第一次 初始化使用
-#git init
-#git pull
-#git add -A
-#git commit -m '上传最新代码'
+git pull
+git add -A
+git commit -m '更新功能'
 
-#git push origin master
+git push origin master
 
 # 生成静态文件
-#npm run docs:build
+npm run docs:build
 
 # 进入生成的文件夹
 cd dist
 
-# 第一次初始化项目 如果是发布到自定义域名 打开
+# 如果是发布到自定义域名
 echo 'www.docs.isryan.com' > CNAME
 
-#第一次 初始化使用
 #git init
-git pull
-
-git add -A
-git commit -m 'deploy'
+#git add -A
+#git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-#git push -f git@github.com:DaQiangSu/vuepress-docs.git master:gh-pages
-git push origin master:gh-pages
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 cd -
