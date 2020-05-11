fd8e7888f75cb9b67ee26373b945ab6e2439ed55
DaQiangSu <13683804270@163.com>
2020-05-11 16:37:38 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  base: '/docs/',
+  //base: '/vuepress-docs/',
   title: '技术文档',
   dest: './dist',
   description: '一个记录文档, 认知, 见解的技术文档网站',
@@ -15,33 +15,24 @@ module.exports = {
         ]
       },
       { text: '开发工具', link: '/development-tool/' },
-      { text: '惊喜彩蛋', link: '/easter-egg/' },
+      //{ text: '惊喜彩蛋', link: '/easter-egg/' },
       { text: '关联网址', link: 'http://www.isryan.com' },
     ],
     sidebar: {
       '/docker/': [{
           title: '主题',
           collapsable: false,
-          children: [
-            { title: '介绍', path:'/docker/'},
-            { title: '使用主题', path: 'one' }
-          ]
+          children: ["","one","tow"]
         }
       ],
       '/springcloud/': [{
         title: '主题',
         collapsable: false,
-        children: [
-          { title: '介绍', path:'/springcloud/'},
-          { title: '使用主题', path: 'one' },
-          { title: '默认主题', path: 'default' }
-        ]
+        children: ["","one","tow","default"]
       }]
     },
     sidebarDepth: 10,
     lastUpdated: '上次更新', // string | boolean,
-    displayAllHeaders: true, // 默认值：false
-    activeHeaderLinks: true, // 默认值：true
     extend: '@vuepress/theme-default'
   }
 }
